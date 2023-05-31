51cb8546aa45d009acbf9ecf6f312a3b58feb8e0
Mikaelwendin <mikael.wendin83@gmail.com>
2023-05-31 19:41:24 +0200

    (feat): Added working state for selected date. Also disabled earlier dates

--- fed22s-therestaurant/src/components/BookTable/BookTable.tsx
+++ fed22s-therestaurant/src/components/BookTable/BookTable.tsx
@@ -1,20 +1,28 @@
-import { FormEvent } from "react"
+import { ChangeEvent, ChangeEventHandler, FormEvent, useState } from "react"
 import Calendar from "react-calendar";
+import { Value } from "react-calendar/dist/cjs/shared/types";
 
 export const BookTable = () => {
+
+
+    const [userInput, setUserInput] = useState("");
+    const [dateState, setDateState] = useState(new Date());
 
     const handleSubmit = (e: FormEvent) => {
         e.preventDefault();
 
      }
-
-
-
+     const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
+        setUserInput(e.target.value);
+     }
+    
+     console.log(dateState)
+     console.log(userInput)
     return <>
         <div className="bookingBox">
         <form onSubmit={handleSubmit}>
-            <Calendar></Calendar>
-            <select name="numberOfPeople">
+            <Calendar value={dateState} onClickDay={setDateState} minDate={new Date()}></Calendar>
+            <select name="numberOfPeople" value={userInput} onChange={handleChange}>
             <option value=""> Please select number of guests</option>
             <option value="1">1</option>
             <option value="2">2</option>
@@ -23,6 +31,7 @@ export const BookTable = () => {
             <option value="5">5</option>
             <option value="6">6</option>
             </select>
+            <button>Go</button>
         </form>
         </div>
 
