51b5d1b4e15248e359f20475bd8075d4580120d9
maxtkarlsson <113439377+maxtkarlsson@users.noreply.github.com>
2023-05-31 15:10:40 +0200

    (feat): Add mongoose schema for Booking

--- fed22s-backend/src/models/Booking.js
+++ fed22s-backend/src/models/Booking.js
@@ -1,3 +1,41 @@
 const mongoose = require("mongoose");
 
-const BookingSchema = new mongoose.Schema({});
+const CustomerSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+});
+
+const BookingSchema = new mongoose.Schema({
+  date: {
+    type: String,
+    required: true,
+  },
+  time: {
+    type: String,
+    required: true,
+  },
+  numberOfGuests: {
+    type: Number,
+    default: 1,
+    min: 1,
+    max: 6,
+    required: true,
+  },
+  customer: {
+    type: CustomerSchema,
+  },
+});
+
+module.exports = mongoose.model("Booking", BookingSchema);
