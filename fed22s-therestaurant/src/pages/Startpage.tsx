e06051554f7ed5e8b21c6a34fe3aeb2884090aa3
coraliney <carro_gryglas@hotmail.com>
2023-06-01 04:36:39 +0200

    (feat:) changing popup modal

--- fed22s-therestaurant/src/pages/Startpage.tsx
+++ fed22s-therestaurant/src/pages/Startpage.tsx
@@ -1,48 +1,8 @@
-import { useEffect, useState } from "react";
-import { BookTable } from "../components/BookTable/BookTable";
-
-export const Startpage = () => {
-
-  const [isShown, setIsShown] = useState(() => {
-    return (localStorage.getItem("checkbox") || "false") === "false";
-  });
-
-  useEffect(() => {
-    localStorage.setItem("checkbox", String(isShown));
-  }, [isShown]);
-
-  const handleClick = () => {
-    setIsShown(true)
-
-  }
-  return <>
-    {!isShown && (
-      <button onClick={handleClick}>Boka Bord</button>)}
-    {isShown && (<BookTable></BookTable>)}
-  </>;
-
-};
-
-
-export const Bookingpage = () => {
-
-  const [isShown, setIsShown] = useState(false);
-
-  const handleClick = () => {
-    setIsShown(true)
-
-  }
-  return <>
-    {!isShown && (
-      <button onClick={handleClick}>Boka Bord</button>)}
-    {isShown && (<BookTable></BookTable>)}
-  </>;
-
-}
 
 
 //varför kallar den inte tillbaka till HOME efter att man har klickat på boka?
 
-//export const Startpage = () => {
-//  return <>Startpage is working</>;
-//};
+export const Startpage = () => {
+  return <>
+    Startpage is working</>;
+};
