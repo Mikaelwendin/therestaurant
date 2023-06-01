a082c391768ea240ec825c115337978995f1eb27
coraliney <carro_gryglas@hotmail.com>
2023-06-01 02:38:13 +0200

    (feat): added button on startpage

--- fed22s-therestaurant/src/pages/Startpage.tsx
+++ fed22s-therestaurant/src/pages/Startpage.tsx
@@ -1,3 +1,43 @@
+import { useState } from "react";
+import { BookTable } from "../components/BookTable/BookTable";
+
 export const Startpage = () => {
-  return <>Startpage is working</>;
+
+
+  const [isShown, setIsShown] = useState(false);
+
+  const handleClick = () => {
+    setIsShown(true)
+
+  }
+  return <>
+    {!isShown && (
+      <button onClick={handleClick}>Boka Bord</button>)}
+    {isShown && (<BookTable></BookTable>)}
+  </>;
+
 };
+
+
+export const Bookingpage = () => {
+
+  const [isShown, setIsShown] = useState(false);
+
+  const handleClick = () => {
+    setIsShown(true)
+
+  }
+  return <>
+    {!isShown && (
+      <button onClick={handleClick}>Boka Bord</button>)}
+    {isShown && (<BookTable></BookTable>)}
+  </>;
+
+}
+
+
+
+
+//export const Startpage = () => {
+//  return <>Startpage is working</>;
+//};
