e935e091de6f3ff03d758efbf331772c01a79dcc
maxtkarlsson <113439377+maxtkarlsson@users.noreply.github.com>
2023-06-01 13:16:56 +0200

    (refactor): Add more testdata to seedDb file

--- fed22s-backend/seedDb/bookings.js
+++ fed22s-backend/seedDb/bookings.js
@@ -1,12 +1,125 @@
+//Thu Jun 15 2023: 88 persons booked
+//Thu Jun 16 2023: 19 persons booked
+
 exports.mockBookingData = [
   {
-    date: "testdate",
-    time: "testtime",
+    date: "Thu Jun 15 2023",
+    time: "test-time",
     numberOfGuests: 5,
     customer: {
-      name: "testname",
-      email: "testemail",
-      phone: "testphone",
+      name: "name1",
+      email: "mail1",
+      phone: "phone1",
+    },
+  },
+  {
+    date: "Thu Jun 15 2023",
+    time: "test-time",
+    numberOfGuests: 10,
+    customer: {
+      name: "name2",
+      email: "mail2",
+      phone: "phone2",
+    },
+  },
+  {
+    date: "Thu Jun 15 2023",
+    time: "test-time",
+    numberOfGuests: 5,
+    customer: {
+      name: "name3",
+      email: "mail3",
+      phone: "phone3",
+    },
+  },
+  {
+    date: "Thu Jun 15 2023",
+    time: "test-time",
+    numberOfGuests: 20,
+    customer: {
+      name: "name4",
+      email: "mail4",
+      phone: "phone4",
+    },
+  },
+  {
+    date: "Thu Jun 15 2023",
+    time: "test-time",
+    numberOfGuests: 10,
+    customer: {
+      name: "name5",
+      email: "mail5",
+      phone: "phone5",
+    },
+  },
+  {
+    date: "Thu Jun 15 2023",
+    time: "test-time",
+    numberOfGuests: 10,
+    customer: {
+      name: "name5",
+      email: "mail5",
+      phone: "phone5",
+    },
+  },
+  {
+    date: "Thu Jun 15 2023",
+    time: "test-time",
+    numberOfGuests: 10,
+    customer: {
+      name: "name6",
+      email: "mail6",
+      phone: "phone6",
+    },
+  },
+  {
+    date: "Thu Jun 15 2023",
+    time: "test-time",
+    numberOfGuests: 15,
+    customer: {
+      name: "name7",
+      email: "mail7",
+      phone: "phone7",
+    },
+  },
+  {
+    date: "Thu Jun 15 2023",
+    time: "test-time",
+    numberOfGuests: 3,
+    customer: {
+      name: "name8",
+      email: "mail8",
+      phone: "phone8",
+    },
+  },
+  {
+    date: "Thu Jun 16 2023",
+    time: "test-time",
+    numberOfGuests: 3,
+    customer: {
+      name: "name9",
+      email: "mail9",
+      phone: "phone9",
+    },
+  },
+  {
+    date: "Thu Jun 16 2023",
+    time: "test-time",
+    numberOfGuests: 10,
+    customer: {
+      name: "name10",
+      email: "mail10",
+      phone: "phone10",
+    },
+  },
+  {
+    date: "Thu Jun 16 2023",
+    time: "test-time",
+    numberOfGuests: 6,
+    customer: {
+      name: "name11",
+      email: "mail11",
+      phone: "phone11",
     },
   },
 ];
