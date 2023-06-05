a4e55fc83a014649d4b76c5e4c08c9f37e815240
maxtkarlsson <113439377+maxtkarlsson@users.noreply.github.com>
2023-06-05 15:13:09 +0200

    (feat): Added all API calls to BookingService

--- fed22s-therestaurant/src/services/BookingService.ts
+++ fed22s-therestaurant/src/services/BookingService.ts
@@ -8,10 +8,42 @@ const get = async <T>(endpoint: string) => {
   return response.data;
 };
 
+const post = async <T>(endpoint: string, data: T) => {
+  const response = await axios.post<T>(`${BASE_URL}${endpoint}`, data);
+  return response.data;
+};
+
+const put = async <T>(endpoint: string, data: T) => {
+  const response = await axios.put<T>(`${BASE_URL}${endpoint}`, data);
+  return response.data;
+};
+
+const del = async (endpoint: string) => {
+  const response = await axios.delete(`${BASE_URL}${endpoint}`);
+  return response.data;
+};
+
 export const getAllBookings = async (): Promise<IBooking[]> => {
   return await get<IBooking[]>("");
 };
 
-export const getBookingById = async (id: number): Promise<IBooking> => {
+export const getBookingById = async (id: string): Promise<IBooking> => {
   return await get<IBooking>(`/${id}`);
+};
+
+export const createNewBooking = async (
+  newBooking: IBooking
+): Promise<IBooking> => {
+  return await post<IBooking>("", newBooking);
+};
+
+export const updateBookingById = async (
+  id: string,
+  booking: IBooking
+): Promise<IBooking> => {
+  return await put<IBooking>(`/${id}`, booking);
+};
+
+export const deleteBookingById = async (id: string) => {
+  return await del(`/${id}`);
 };
