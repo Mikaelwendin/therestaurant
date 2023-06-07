cbc94fd321666fc22fef82b367aa5a03448988fa
coraliney <carro_gryglas@hotmail.com>
2023-06-07 13:24:25 +0200

    (feat): adding connection between front-adn backend

--- fed22s-therestaurant/src/pages/Bookingpage.tsx
+++ fed22s-therestaurant/src/pages/Bookingpage.tsx
@@ -1,13 +1,37 @@
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { BookTable } from "../components/BookTable/BookTable";
 import { CancelTable } from "../components/CancelTable/CancelTable";
+import axios from "axios";
 
 
 export const Bookingpage = () => {
 
     const [isBooking, setIsShown] = useState(false);
     const [isCancel, setIsCancel] = useState(false);
+
+    const [bookingSucess, setSucessBooking] = useState({
+        guest: 0,
+        seating: "",
+        mobile: "",
+
+    });
+
+    const bookingUrl = "http://localhost:4000";
+
+    const avaliableSeat = async () => {
+        try {
+            const res = await axios.get(bookingUrl + "/avaliableSeat"), {
+                params: {
+                    guest: bookingSucess.guest,
+                    seating: bookingSucess.seating,
+                },
+            };
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
 
     return <>
         {!isBooking && !isCancel && (
