154d2512e8644d403acecd5e44d850e9b4cc2a4e
coraliney <carro_gryglas@hotmail.com>
2023-06-12 13:04:31 +0200

    (feat): added go back option

--- fed22s-therestaurant/src/components/CancelTable/CancelTable.tsx
+++ fed22s-therestaurant/src/components/CancelTable/CancelTable.tsx
@@ -2,6 +2,7 @@ import { ChangeEvent, FormEvent, useState } from "react";
 import { Confirmation } from "../Confirmation/Confirmation";
 import { deleteBookingById, getBookingById } from "../../services/BookingService";
 import { IBooking, defaultBooking } from "../../models/IBooking";
+import { Link } from "react-router-dom";
 
 export const CancelTable = () => {
 
@@ -9,7 +10,7 @@ export const CancelTable = () => {
     const [isShown, setIsShown] = useState(false);
     const [bookingCanceled, setBookingCanceled] = useState(false);
     const [booking, setBooking] = useState<IBooking>(defaultBooking);
-    
+
     const deleteBooking = () => {
         deleteBookingById(bookingId)
         setBookingCanceled(true)
@@ -21,18 +22,18 @@ export const CancelTable = () => {
 
         setBookingId(e.target.value)
     }
-    const handleSubmit = async (e:FormEvent) => {
+    const handleSubmit = async (e: FormEvent) => {
 
         e.preventDefault();
         setBooking(await getBookingById(bookingId))
         setIsShown(true)
 
     }
-    return  <>{!isShown && (
-    <div className="cancelTable">
-    <h1>Avboka bord</h1>
+    return <>{!isShown && (
+        <div className="cancelTable">
+            <h1>Avboka bord</h1>
 
-    <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit}>
                 <input
                     type="text"
                     value={bookingId}
@@ -41,21 +42,24 @@ export const CancelTable = () => {
                 />
                 <button>Sök</button>
             </form>
-    </div> 
+        </div>
     )}
-{isShown && !bookingCanceled && (<div>
-    <h2>Din bokning:</h2>
-    <ul>
-        <li>Namn: {booking.customer.name}</li>
-        <li>Datum: {booking.date}</li>
-        <li>Tid: {booking.time}</li>
-        <li>Antal gäster: {booking.numberOfGuests}</li>
-        <li>Email: {booking.customer.email}</li>
-    </ul>
-    <button onClick={deleteBooking}>Ta bort bokning</button>
-</div>)}
-{bookingCanceled && (<div>
-<Confirmation name={booking.customer.name} msg={"Ditt bord är nu avbokat"}></Confirmation>
-</div>)}
-</>
+        {isShown && !bookingCanceled && (<div>
+            <h2>Din bokning:</h2>
+            <ul>
+                <li>Namn: {booking.customer.name}</li>
+                <li>Datum: {booking.date}</li>
+                <li>Tid: {booking.time}</li>
+                <li>Antal gäster: {booking.numberOfGuests}</li>
+                <li>Email: {booking.customer.email}</li>
+            </ul>
+            <button onClick={deleteBooking}>Ta bort bokning</button>
+
+            <p><Link to="/">Gå tillbaka</Link></p>
+
+        </div>)}
+        {bookingCanceled && (<div>
+            <Confirmation name={booking.customer.name} msg={"Ditt bord är nu avbokat"}></Confirmation>
+        </div>)}
+    </>
 }
