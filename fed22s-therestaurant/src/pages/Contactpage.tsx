bd7a3e54a23d5a55b4f27f87c0405f320d635d57
coraliney <carro_gryglas@hotmail.com>
2023-06-15 12:26:19 +0200

    (feat): contactpage running soon

--- fed22s-therestaurant/src/pages/Contactpage.tsx
+++ fed22s-therestaurant/src/pages/Contactpage.tsx
@@ -1,16 +1,18 @@
 import ContactForm from "../components/Contact/ContactForm";
 import { StyledForm } from "../components/Styled/StyledForm";
-
 import map from "./../assets/photos/map.jpeg"
+import "./ContactPage.scss"
 
 export const Contactpage = () => {
   return (
     <>
-      <div className="form-image-container">
+      <div className="container">
         <form className="styled-form">
           <ContactForm></ContactForm>
         </form>
-        <img src={map} alt="Pinned map" />
+        <div className="form-image-container">
+          <img src={map} alt="Pinned map" />
+        </div>
       </div>
     </>
   );
