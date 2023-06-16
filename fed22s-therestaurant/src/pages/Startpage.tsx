591ea9d5b1d44c7f0a1d4a1df0b328143045d771
Mikaelwendin <mikael.wendin83@gmail.com>
2023-06-16 08:54:17 +0200

    (fix): Final tweaks

--- fed22s-therestaurant/src/pages/Startpage.tsx
+++ fed22s-therestaurant/src/pages/Startpage.tsx
@@ -15,7 +15,7 @@ export const Startpage = () => {
           <div className="rubrik">
             <h1 className="fade-in-heading">The Local Host</h1>
           </div>
-          <div>
+          <div className='presentationBox'>
             <p>Vi hälsar dig hjärtligt välkommen till vår restaurang. Ta plats och njut av en kulinarisk upplevelse som kommer att tillfredsställa dina smaklökar. Vi är här för att göra din tid hos oss minnesvärd och trivsam. Välkommen till en gastronomisk resa fylld av omsorgsfullt tillagade rätter och en välkomnande atmosfär.
             </p>
             <p>Varmt välkommen till The Local Host!</p>
