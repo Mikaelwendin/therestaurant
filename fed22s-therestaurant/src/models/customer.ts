b45fa7464156f397715dc6f038c47dfa123edef9
Mikaelwendin <113438584+Mikaelwendin@users.noreply.github.com>
2024-03-22 15:58:21 +0100

    Delete fed22s-therestaurant/src/models/customer.ts

--- fed22s-therestaurant/src/models/customer.ts
+++
@@ -1,9 +0,0 @@
-/* export class Customer {
-    constructor(
-        public name: string,
-        public email: string,
-        public phone: string
-    )
-    {}
-}
-export const defaultCustomer = new Customer("","",""); */
